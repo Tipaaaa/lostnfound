840b370cca96bab5a1eddd37c1e822b2f3e47169
Hasya Rona Amirahmi <hasyarona1711@gmail.com>
2021-11-27 17:19:26 +0700

    intent di menu profile activity

--- app/src/main/java/com/example/lost_found/ProfileActivity.java
+++ app/src/main/java/com/example/lost_found/ProfileActivity.java
@@ -2,7 +2,10 @@ package com.example.lost_found;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void toHistory(View view){
+        Toast.makeText(this, "Anda Memasuki Page History", Toast.LENGTH_SHORT).show();
+        //Intent historyIntent = new Intent(this, HistoryActivity.class);
+    }
+
+    public void toFoundItem(View view){
+        Toast.makeText(this, "Anda Memasuki Page Found Item", Toast.LENGTH_SHORT).show();
+    }
+
+    public void toAccount(View view){
+        Toast.makeText(this, "Anda Memasuki Page Account", Toast.LENGTH_SHORT).show();
+    }
+
+    public void toChangePass(View view){
+        Toast.makeText(this, "Anda Memasuki Page Change Password", Toast.LENGTH_SHORT).show();
     }
 }
