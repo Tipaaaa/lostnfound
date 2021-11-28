69ea052b145d44e00608721774f0f9be0ba7f0e4
Tipaaaa <lathifa.hardi11@gmail.com>
2021-11-28 19:43:43 +0700

    intent to activity found item

--- app/src/main/java/com/example/lost_found/FounditemActivity.java
+++ app/src/main/java/com/example/lost_found/FounditemActivity.java
@@ -1,16 +1,92 @@
 package com.example.lost_found;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.lost_found.R;
+import com.example.lost_found.adapter.BarangAdapter;
+import com.example.lost_found.model.Barang;
 
-public class FounditemActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class FounditemActivity extends AppCompatActivity implements BarangAdapter.OnBarangHolderClickListener {
+
+    RecyclerView rvFoundBarang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_founditem);
+
+        BarangAdapter adapterFI = new BarangAdapter();
+        adapterFI.setListData(getBarang());
+        adapterFI.setListener(this);
+
+        rvFoundBarang = findViewById(R.id.rvFoundBarang);
+        rvFoundBarang.setAdapter(adapterFI);
+
+        RecyclerView.LayoutManager managerfounditem = new GridLayoutManager(this, 2);
+        rvFoundBarang.setLayoutManager(managerfounditem);
+
+
+    }
+
+    public ArrayList<Barang> getBarang(){
+        ArrayList<Barang> listBarang = new ArrayList<Barang>();
+        String nama;
+        String Kategori;
+        String details = "Details";
+        listBarang.add(new Barang(
+                nama = "Tumbler",
+                Kategori = "Lain-lain",
+                details));
+        listBarang.add(new Barang(
+                nama = "Tas Converse",
+                Kategori = "Tas",
+                details));
+        listBarang.add(new Barang(
+                nama = "Laptop HP Envy",
+                Kategori = "Elektronik",
+                details));
+        listBarang.add(new Barang(
+                nama = "Tas 'False Pretense'",
+                Kategori = "Tas",
+                details));
+        listBarang.add(new Barang(
+                nama = "Kaos Merah Maroon",
+                Kategori = "Pakaian",
+                details));
+        listBarang.add(new Barang(
+                nama = "Converse Putih",
+                Kategori = "Sepatu",
+                details));
+        listBarang.add(new Barang(
+                nama = "Jam DW Coklat",
+                Kategori = "Pakaian",
+                details));
+        listBarang.add(new Barang(
+                nama = "iPhone 11 Pro",
+                Kategori = "Elektronik",
+                details));
+        listBarang.add(new Barang(
+                nama = "Jaket Hitam",
+                Kategori = "Pakaian",
+                details));
+        listBarang.add(new Barang(
+                nama = "Dompet Coklat",
+                Kategori = "Pakaian",
+                details));
+
+
+        return listBarang;
+    }
+
+    @Override
+    public void onClick(View v, Barang barang) {
+
     }
 }
